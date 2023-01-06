0556cb175947231bf54fefd1b0585b7dbac215dd
JoelBenton <91064361+JoelBenton@users.noreply.github.com>
2023-01-06 11:37:09 +0000

    Update Requirement4-3.sql
    Miss read requirement for studio name instead of ID

--- Requirement4-3.sql
+++ Requirement4-3.sql
@@ -1,8 +1,8 @@
 #Working
 
-SELECT p."ProjectID", p."Name", s."Name", p."Status" 
-FROM bohemia.studioinformation s
-left join bohemia.studio s2 on s."StudioInformationID" = s2."StudioInformationID"
-left join bohemia.studioassignment s3 on s2."StudioID" = s3."StudioID"
+SELECT p."ProjectID", p."Name", s2."Name", p."Status" 
+FROM bohemia.studio s
+left join bohemia.studioinformation s2 on s."StudioInformationID" = s2."StudioInformationID"
+left join bohemia.studioassignment s3 on s."StudioID" = s3."StudioID"
 left join bohemia.project p on s3."ProjectID" = p."ProjectID"
-WHERE s."Name" = 'Fuzzy Sheep Studios';
+WHERE s."StudioID"  = '1';
