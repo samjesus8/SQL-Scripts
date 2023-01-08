168e0782f0520ae8cba111c06d9420e695042cae
Joel Benton <joelbenton1903@gmail.com>
2023-01-08 14:05:43 +0000

    Updated Requirement 4-1  - Made it so date will always be 1 year ago from script run date

--- Requirement4-1.sql
+++ Requirement4-1.sql
@@ -4,5 +4,5 @@ SELECT e."UserName", e."FirstName", e."Lastname", e."Email", c."StartDate"
 FROM employeeinformation e
 left join employee e2 on e2."EmployeeInfoID" = e."EmployeeInformationID"
 left join contract c on c."ContractID" = e2."EmployeeID"
-where c."StartDate" > '01/08/22'
+where c."StartDate" > (current_date - 365)
 order by (c."StartDate") desc;
